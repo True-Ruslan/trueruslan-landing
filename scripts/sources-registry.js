0b0ba4f7b0fbd840917231973464ffedd2b326bc
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-07-22 14:49:37 +0300

    feat: add Sources Registry module surface

---
+++ scripts/sources-registry.js
@@ -0,0 +1,30 @@
+export const SOURCE_TYPE_VALUES = Object.freeze([
+  'article',
+  'documentation',
+  'book',
+  'course',
+  'talk',
+  'blog',
+  'paper',
+  'other',
+]);
+
+export function validateSourcesRegistry() {
+  throw new Error('Sources Registry validation is not implemented yet.');
+}
+
+export function loadSourcesRegistry() {
+  throw new Error('Sources Registry loading is not implemented yet.');
+}
+
+export function sortSources() {
+  throw new Error('Sources Registry sorting is not implemented yet.');
+}
+
+export function renderSourcesKnowledgeBase() {
+  throw new Error('Sources Knowledge Base rendering is not implemented yet.');
+}
+
+export function applySourcesKnowledgeBase() {
+  throw new Error('Sources Knowledge Base build injection is not implemented yet.');
+}
